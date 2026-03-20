e63fb80a28c2e7b6573ab50f2ed2ee7cd5407ca8
Nazar Bekerskij <bekerskijnazar9@gmail.com>
2026-03-20 15:12:03 +0200

    123

--- src/app.js
+++ src/app.js
@@ -1,3 +1,78 @@
+// const firstRef = document.querySelector('#firstName');
+// const lastRef = document.querySelector('#lastName')
+// const phoneRef = document.querySelector('#phone')
+// const emailRef = document.querySelector('#email')
+// const btnRef = document.querySelector('#saveBtn')
+// const listRef = document.querySelector(".list")
+
+
+// let contacts = JSON.parse(localStorage.getItem("contacts")) || [];
+
+
+// function render () {
+//     const markup = contacts.map((contact, index) => {
+//         return `<li>
+//             <strong>${contact.firstName} ${contact.lastName}</strong>: ${contact.phone}
+//             <button data-index="${index}" class="delete-btn">Видалити</button>
+//             <button data-index="${index}" class="edit-btn">Редагувати</button>
+//         </li>`;
+//     }).join("")
+//     listRef.innerHTML = markup
+// }
+// render()
+
+
+// btnRef.addEventListener("click", () => {
+//     const newContact = {
+//         firstName: firstRef.value,
+//         lastName: lastRef.value,
+//         phone: phoneRef.value,
+//         email: emailRef.value,
+//     }
+//     contacts.push(newContact)
+
+//     localStorage.setItem("contacts", JSON.stringify(contacts));
+//     render()
+
+//     firstRef.value = "";
+//     lastRef.value = "";
+//     phoneRef.value = "";
+//     emailRef.value = "";
+// })
+
+
+// listRef.addEventListener("click", (event) => {
+
+//     if(event.target.classList.contains("delete-btn")){
+//         const index = event.target.dataset.index
+//         contacts.splice(index, 1)
+//         localStorage.setItem("contacts", JSON.stringify(contacts))
+//         render()
+//     }
+//     if(event.target.classList.contains("edit-btn")){
+//         const index = event.target.dataset.index
+//         const contact = contacts[index];
+
+
+//         firstRef.value = contact.firstName;
+//         lastRef.value = contact.lastName;
+//         phoneRef.value = contact.phone;
+//         emailRef.value = contact.email;
+
+
+//         contacts.splice(index, 1);
+
+//         localStorage.setItem("contacts", JSON.stringify(contacts))
+//         render()
+//     }
+// })
+
+
+///////////////////////////////////////////////////////////////////
+
+
+
+
 const firstRef = document.querySelector('#firstName');
 const lastRef = document.querySelector('#lastName')
 const phoneRef = document.querySelector('#phone')
@@ -5,17 +80,21 @@ const emailRef = document.querySelector('#email')
 const btnRef = document.querySelector('#saveBtn')
 const listRef = document.querySelector(".list")
 
-
 let contacts = JSON.parse(localStorage.getItem("contacts")) || [];
 
 
 function render () {
     const markup = contacts.map((contact, index) => {
-        return `<li>
-            <strong>${contact.firstName} ${contact.lastName}</strong>: ${contact.phone}
-            <button data-index="${index}" class="delete-btn">Видалити</button>
-            <button data-index="${index}" class="edit-btn">Редагувати</button>
-        </li>`;
+        return `
+            <li>
+                <strong>${contact.firstName} ${contact.lastName}</strong>
+                <p>Тел: ${contact.phoneRef}</p> 
+                <p>Email: ${contact.emailRef}</p> <div class="actions">
+                    <button data-index="${index}" class="delete-btn">Видалити</button>
+                    <button data-index="${index}" class="edit-btn">Редагувати</button>
+                </div>
+            </li>
+        `;
     }).join("")
     listRef.innerHTML = markup
 }
@@ -26,12 +105,13 @@ btnRef.addEventListener("click", () => {
     const newContact = {
         firstName: firstRef.value,
         lastName: lastRef.value,
-        phone: phoneRef.value,
-        email: emailRef.value,
+        phoneRef: phoneRef.value,
+        emailRef: emailRef.value,
     }
     contacts.push(newContact)
 
-    localStorage.setItem("contacts", JSON.stringify(contacts));
+    localStorage.setItem("contacts", JSON.stringify(contacts))
+
     render()
 
     firstRef.value = "";
@@ -41,29 +121,55 @@ btnRef.addEventListener("click", () => {
 })
 
 
-listRef.addEventListener("click", (event) => {
 
+listRef.addEventListener("click", (event) => {
     if(event.target.classList.contains("delete-btn")){
-        const index = event.target.dataset.index
+        const index = event.target.dataset.index;
+
         contacts.splice(index, 1)
+
         localStorage.setItem("contacts", JSON.stringify(contacts))
         render()
     }
+
     if(event.target.classList.contains("edit-btn")){
-        const index = event.target.dataset.index
-        const contact = contacts[index];
+        const index = event.target.dataset.index;
+        const contactToEdit = contacts[index];
 
+        firstRef.value = contactToEdit.firstName;
+        lastRef.value = contactToEdit.lastName;
+        phoneRef.value = contactToEdit.phoneRef;
+        emailRef.value = contactToEdit.emailRef;
 
-        firstRef.value = contact.firstName;
-        lastRef.value = contact.lastName;
-        phoneRef.value = contact.phone;
-        emailRef.value = contact.email;
-
-
-        contacts.splice(index, 1);
+        contacts.splice(index, 1)
 
         localStorage.setItem("contacts", JSON.stringify(contacts))
         render()
     }
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
